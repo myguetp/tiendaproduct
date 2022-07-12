5bdfc7d21d55ecf8f44f48f9fccfca26e7e425fa
miguel <jmtiradop19@gmail.com>
2022-07-12 11:01:27 -0500

    el login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ClienteService } from 'src/app/services/cliente.service';
+declare var iziToast:any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user : any = {};
+  public usuario: any = {};
+  public token: any = {};
+
+  constructor(
+    private _clienteService : ClienteService,
+    private _router: Router
+  ) {
+    //this.token = this._clienteService.getToken();
+  }
 
   ngOnInit(): void {
+   /* if(this.token){
+      this._router.navigate(['/']);
+    }else{
+
+    }*/
+  }
+
+  login(loginForm:any){
+    //validamos
+    if(loginForm.valid){
+      console.log(this.user);
+
+
+      let data = {
+        email: this.user.email,
+        password: this.user.password
+      }
+
+      this._clienteService.login_cliente(data).subscribe(
+        response => {
+          if(response.data == undefined){
+            iziToast.show({
+              title: 'Error',
+              titleColor: '#FF0000',
+              color: '#FFF',
+              class: 'text-danger',
+              position: 'topLeft',
+              message: response.messege
+            });
+          }else{
+            this.usuario = response.data;
+            localStorage.setItem('token',response.token);
+            localStorage.setItem('_id',response.data._id);
+
+            this._router.navigate(['/']);
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      )
+
+    }else{
+      iziToast.show({
+        title: 'Error',
+        titleColor: '#FF0000',
+        color: '#FFF',
+        class: 'text-danger',
+        position: 'topLeft',
+        message: 'datos no validos'
+      })
+    }
   }
 
 }
---
+++ src/app/services/GLOBAL.ts
@@ -0,0 +1,4 @@
+//enlace backend
+export let GLOBAL = {
+  url: 'http://127.0.0.1:9000/api/',
+}
